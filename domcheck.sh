3598bf7a646415753c6f5a4725e3b4692c0a5f74
Franck Ridel <68272257+cryptrz@users.noreply.github.com>
2025-01-16 21:04:31 +0100

    Update domcheck.sh
    Added Registrar URL and filter semicolon for TXT record

--- domcheck.sh
+++ domcheck.sh
@@ -9,10 +9,11 @@ report="recon_$1_on_$(date +%F_at_%H:%M:%S).txt"
 
 {
         echo $(curl -I -s $1 | grep Server)
+        whois $1 | grep "Registrar URL"
         host $1
-        dig NS $1 | grep NS | grep -v ";"
-        dig CNAME $1 | grep SOA
-        dig TXT $1 | grep IN
+        dig NS $1  | grep NS | grep -v ";"
+        dig CNAME $1  | grep SOA
+        dig TXT $1  | grep IN | grep -v ";"
 } | tee $report
 
 echo "Report saved in $(pwd)/$report" 
